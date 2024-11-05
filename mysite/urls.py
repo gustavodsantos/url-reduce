99e4b65546a6520cdaae9761d235d82bd6b180ea
gustavodsantos <gustavojuniordos@hotmail.com>
2024-11-04 21:11:19 -0300

    criado setup do projeto url_reduce

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mysite.encurtador import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('<slug:slug>', views.redirecionar),
+    path('relatorios/<slug:slug>', views.relatorios),
 ]
